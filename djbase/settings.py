4f075ca42cc0df2cbc7c620bf2200749ad8191bb
samuel_uft <samuelpf25@gmail.com>
2022-12-07 10:55:58 -0300

    segundo

--- djbase/settings.py
+++ djbase/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'core/static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
